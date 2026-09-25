fb5b39f751741dbe52b922c447c8f16a35c4724e
Laraib Mukhtar <laraibmukhtar440@gmail.com>
2026-09-25 18:16:07 +0500

    Complete portfolio website

--- script.js
+++ script.js
@@ -1,60 +1,40 @@
-// =========================
-// Typing Effect
-// =========================
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.querySelector(".nav-links");
 
-const text = "Frontend Developer";
-let index = 0;
-const typing = document.querySelector(".hero h2");
-function typeEffect(){
-    if(index < text.length){
-        typing.innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeEffect,100);
-    }
-}
-typing.innerHTML = "";
-typeEffect();
-// =========================
-// Navbar Scroll Effect
-// =========================
+menuBtn.addEventListener("click", () => {
 
-const navbar = document.querySelector(".navbar");
-window.addEventListener("scroll",()=>{
-    if(window.scrollY > 50){
-        navbar.style.background = "rgba(11,17,32,0.95)";
-}
-else{
-    navbar.style.background = "rgba(11,17,32,0.75)";
-}
+    navLinks.classList.toggle("mobile-menu");
+
 });
-// =========================
-// Close Mobile Menu
-// =========================
-const navLinks = document.querySelectorAll(".nav-link");
 
-const menu = document.querySelector(".navbar-collapse");
-navLinks.forEach(link=>{
-    link.addEventListener("click",()=>{
-        if(menu.classList.contains("show")){
-            document.querySelector(".navbar-toggler").click();
+
+// Close mobile menu when clicking a link
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("mobile-menu");
+    });
+
+});
+// FAQ Section
+const faqQuestions = document.querySelectorAll(".faq-question");
+
+faqQuestions.forEach((question) => {
+    question.addEventListener("click", () => {
+
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector("i");
+
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+            icon.classList.remove("fa-minus");
+            icon.classList.add("fa-plus");
+        } else {
+            answer.style.display = "block";
+            icon.classList.remove("fa-plus");
+            icon.classList.add("fa-minus");
         }
+
     });
 });
-
-
-
-// =========================
-// Footer Year
-// =========================
-
-const year = new Date().getFullYear();
-
-const copyright = document.querySelector(".copyright p");
-
-
-if(copyright){
-
-    copyright.innerHTML =
-    `© ${year} Laraib Mukhtar. All Rights Reserved.`;
-
-}
